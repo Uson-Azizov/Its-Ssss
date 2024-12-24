2229611ae470a53e9efad4e5a1378fa41a1409b8
Усон-Азизов <teuduscr@gmail.com>
2024-12-24 15:46:38 +0300

    dadadadad adafa

---
+++ Testtttt.py
@@ -0,0 +1,28 @@
+######################### git , github, кидшуу #########################
+
+# 1) git init (инитсилиция гита)
+
+# 2) git add . (перекинуть все файлы) - (либо по название файла)
+
+# 3) git commit -m " " ( можно написать комментарии)
+
+# 4) git branch -M main (ветка)
+
+# 5) git remote add origin https://github.com/Uson-Azizov/Its-Ssss.git
+
+# 6) git push -u origin main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
